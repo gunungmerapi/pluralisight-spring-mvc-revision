775f2fed52d01a8fb46b7026c25809a1e89ed0c7
Wajid Poernomo <Wajid.Poernomo@monash.edu>
2017-10-18 01:42:58 +1100

    Added form tags and interceptor features.

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -14,13 +14,6 @@ public class MinutesController {
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
 		
 		System.out.println("exercise: " + exercise.getMinutes());
-		return "addMinutes.html";
+		return "addMinutes";
 	}
-	
-//	@RequestMapping(value = "/addMoreMinutes")
-//	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
-//		
-//		System.out.println("exercising: " + exercise.getMinutes());
-//		return "addMinutes";
-//	}
 }
